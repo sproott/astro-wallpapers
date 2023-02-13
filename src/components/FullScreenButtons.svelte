f2164d450504d2681267d6219f0b438fa76647de
sproott <david.hrabe12@seznam.cz>
2023-02-13 18:52:15 +0100

    feat: full screen buttons

---
+++ src/components/FullScreenButtons.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let hidden = false;
+
+  const hide = () => (hidden = true);
+  const fullscreen = () => {
+    hide();
+    document.documentElement.requestFullscreen();
+  };
+</script>
+
+<div
+  class:hidden
+  class="absolute inset-0 flex flex-col justify-end items-center gap-4 py-4"
+>
+  <button
+    class:hidden
+    class="bg-nord-frost-400 p-2 rounded"
+    on:click={fullscreen}>Full-screen</button
+  >
+  <button class:hidden class="bg-nord-frost-400 p-2 rounded" on:click={hide}
+    >Hide buttons</button
+  >
+</div>
