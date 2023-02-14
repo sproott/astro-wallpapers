261845ae3e3115bf4304cfe7248949e0d44a4333
sproott <david.hrabe12@seznam.cz>
2023-02-14 10:43:21 +0100

    feat: JetBrains Mono

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,58 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import { presetIcons, presetWind } from "unocss";
+import { presetWind, presetIcons, presetWebFonts } from "unocss";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    presets: [presetWind(), presetIcons({
-      extraProperties: {
-        display: "inline-block",
-        "vertical-align": "middle"
-      }
-    })],
-    theme: {
-      colors: {
-        nord: {
-          polarNight: {
-            100: "#2e3440",
-            200: "#3b4252",
-            300: "#434c5e",
-            400: "#4c566a"
+  integrations: [
+    UnoCSS({
+      presets: [
+        presetWind(),
+        presetIcons({
+          extraProperties: {
+            display: "inline-block",
+            "vertical-align": "middle",
           },
-          snowStorm: {
-            100: "#d8dee9",
-            200: "#e5e9f0",
-            300: "#eceff4"
+        }),
+        presetWebFonts({
+          provider: "bunny",
+          fonts: {
+            mono: ["JetBrains Mono"],
           },
-          frost: {
-            100: "#8fbcbb",
-            200: "#88c0d0",
-            300: "#81a1c1",
-            400: "#5e81ac"
+        }),
+      ],
+      theme: {
+        colors: {
+          nord: {
+            polarNight: {
+              100: "#2e3440",
+              200: "#3b4252",
+              300: "#434c5e",
+              400: "#4c566a",
+            },
+            snowStorm: {
+              100: "#d8dee9",
+              200: "#e5e9f0",
+              300: "#eceff4",
+            },
+            frost: {
+              100: "#8fbcbb",
+              200: "#88c0d0",
+              300: "#81a1c1",
+              400: "#5e81ac",
+            },
+            red: "#bf616a",
+            orange: "#d08770",
+            yellow: "#ebcb8b",
+            green: "#a3be8c",
+            purple: "#b48ead",
           },
-          red: "#bf616a",
-          orange: "#d08770",
-          yellow: "#ebcb8b",
-          green: "#a3be8c",
-          purple: "#b48ead"
-        }
-      }
-    }
-  }), svelte()]
+        },
+      },
+    }),
+    svelte(),
+  ],
 });
