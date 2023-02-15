c2d4d5f0b5849404519b165f1c806e4e47f41804
sproott <david.hrabe12@seznam.cz>
2023-02-15 10:20:22 +0100

    feat: cookies

---
+++ src/pages/[theme].ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+import { themes } from "../lib/constants";
+
+export const post: APIRoute = async ({ cookies, params }) => {
+  const { theme } = params;
+
+  // @ts-ignore
+  if (theme === undefined || !themes.includes(theme)) {
+    return new Response("", {
+      status: 404,
+    });
+  }
+  cookies.set("theme", theme, { path: "/" });
+  return new Response("", {
+    status: 200,
+  });
+};
